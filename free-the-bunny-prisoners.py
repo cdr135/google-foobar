e0b5cd445c52781e5b6c08be07d1a723af837450
cdr135 <cdr135@gmail.com>
2020-08-25 23:38:22 -0700

    rm

--- free-the-bunny-prisoners.py
+++
@@ -1,7 +0,0 @@
-from itertools import combinations
-def solution(num_buns, num_required):
-    b = [[] for i in range(num_buns)]
-    for n,buns in enumerate(combinations(range(num_buns), num_buns-num_required+1)):
-        for i in buns:
-            b[i]+=[n]
-    return b
